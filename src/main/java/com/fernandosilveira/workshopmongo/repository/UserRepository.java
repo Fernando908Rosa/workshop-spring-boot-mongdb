01cf920812bc41a223f3efa521e364fa2ab6caf4
fernandorosa <fernandodarosa908@gmail.com>
2022-03-22 12:12:35 -0300

    Operacao de instanciacao da base do banco de dados

--- src/main/java/com/fernandosilveira/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/fernandosilveira/workshopmongo/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.fernandosilveira.workshopmongo.repository;
 
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,6 @@ import com.fernandosilveira.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
 
 }
