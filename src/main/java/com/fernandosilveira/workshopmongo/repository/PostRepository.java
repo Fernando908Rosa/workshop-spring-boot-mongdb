5a283fac9f3266abc5cab0f2fc3b385d15c5d6f5
fernandorosa <fernandodarosa908@gmail.com>
2022-03-28 15:21:25 -0300

    Consulta simples com query methods

--- src/main/java/com/fernandosilveira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fernandosilveira/workshopmongo/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.fernandosilveira.workshopmongo.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,10 @@ import com.fernandosilveira.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContaining(String text);
 }
+
+
+
+
